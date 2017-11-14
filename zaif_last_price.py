debb0b0d8d4ceddd6d0b98e2a7ec68fa9b4b314a
akihironemoto <akihiro.nemoto.06@gmail.com>
2017-11-14 22:37:07 +0900

    first commit

---
+++ zaif_last_price.py
@@ -0,0 +1,19 @@
+import zaifapi
+from zaifapi import *
+
+if __name__ == '__main__':
+    zaif = ZaifPublicApi()
+
+    zaif_btc_jpy  = zaif.last_price(('btc_jpy'))
+    zaif_xem_jpy  = zaif.last_price(('xem_jpy'))
+    zaif_eth_jpy  = zaif.last_price(('eth_jpy'))
+    zaif_mona_jpy = zaif.last_price(('mona_jpy'))
+    zaif_bch_jpy  = zaif.last_price(('bch_jpy'))
+    zaif_zaif_jpy = zaif.last_price(('zaif_jpy'))
+
+    print('btc_jpy  = ', zaif_btc_jpy['last_price'])
+    print('xem_jpy  = ', zaif_xem_jpy['last_price'])
+    print('eth_jpy  = ', zaif_eth_jpy['last_price'])
+    print('mona_jpy = ', zaif_mona_jpy['last_price'])
+    print('bch_jpy  = ', zaif_bch_jpy['last_price'])
+    print('zaif_jpy = ', zaif_zaif_jpy['last_price'])
